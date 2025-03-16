cf0b318129703f9ad9657a3d868b70eaa33c918e
felmanc <felmanc@gmail.com>
2025-03-16 19:13:01 -0300

    Projeto inicial - Application

---
+++ code/ppaysimplificado/src/main/java/br/com/felmanc/ppaysimplificado/PpaysimplificadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.felmanc.ppaysimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpaysimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpaysimplificadoApplication.class, args);
+	}
+
+}
