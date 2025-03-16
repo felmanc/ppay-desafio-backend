988611e9e81e0030d0fd86952ddbe90eed2d88ef
felmanc <felmanc@gmail.com>
2025-03-16 19:10:41 -0300

    Projeto inicial

---
+++ code/ppaysimplificado/src/test/java/br/com/felmanc/ppaysimplificado/PpaysimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felmanc.ppaysimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpaysimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
