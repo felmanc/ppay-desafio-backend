4610664a6ea6ed4bd464523b2f650957b8ce73c1
felmanc <felmanc@gmail.com>
2025-03-19 14:55:31 -0300

    Tratamento de exceção

--- code/ppaysimplificado/src/main/java/br/com/felmanc/ppaysimplificado/exceptions/GlobalExceptionHandler.java
+++ code/ppaysimplificado/src/main/java/br/com/felmanc/ppaysimplificado/exceptions/GlobalExceptionHandler.java
@@ -1,13 +1,14 @@
 package br.com.felmanc.ppaysimplificado.exceptions;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.Map;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -44,6 +45,18 @@ public class GlobalExceptionHandler {
 	            ));
 	}
 
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
+	    return ResponseEntity
+	            .status(HttpStatus.BAD_REQUEST)
+	            .body(Map.of(
+	                    "error", "BAD_REQUEST",
+	                    "message", "O parâmetro '" + ex.getName() + "' é inválido. Esperado: " + ex.getRequiredType().getSimpleName(),
+	                    "details", ex.getMessage()
+	            ));
+	}
+
+	
 	@ExceptionHandler(IllegalArgumentException.class)
 	public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
 	    return ResponseEntity
