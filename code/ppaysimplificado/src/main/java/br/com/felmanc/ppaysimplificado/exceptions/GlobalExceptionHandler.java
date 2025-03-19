c9641460295b14e90f0aedc1e584083360792e22
felmanc <felmanc@gmail.com>
2025-03-19 09:17:43 -0300

    Tratamento de exceção

--- code/ppaysimplificado/src/main/java/br/com/felmanc/ppaysimplificado/exceptions/GlobalExceptionHandler.java
+++ code/ppaysimplificado/src/main/java/br/com/felmanc/ppaysimplificado/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package br.com.felmanc.ppaysimplificado.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,48 +11,58 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<Object> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+	    return ResponseEntity
+	            .status(HttpStatus.BAD_REQUEST)
+	            .body(Map.of(
+	                    "error", "BAD_REQUEST",
+	                    "message", "Entrada inválida: " + ex.getLocalizedMessage(),
+	                    "details", "A entrada fornecida não pôde ser analisada. Por favor, verifique o formato da entrada e tente novamente."
+	            ));
+	}
 
-    @ExceptionHandler(UnauthorizedTransactionException.class)
-    public ResponseEntity<Object> handleUnauthorizedTransactionException(UnauthorizedTransactionException ex) {
-        return ResponseEntity
-                .status(HttpStatus.FORBIDDEN)
-                .body(Map.of(
-                        "error", "FORBIDDEN",
-                        "message", ex.getMessage(),
-                        "details", "Transação não autorizada pelo serviço externo."
-                ));
-    }	
-	
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<Object> handleMissingRequestParameter(MissingServletRequestParameterException ex) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of(
-                        "error", "BAD_REQUEST",
-                        "message", String.format("O parâmetro '%s' é obrigatório e não foi fornecido.", ex.getParameterName()),
-                        "details", ex.getMessage()
-                ));
-    }
+	@ExceptionHandler(UnauthorizedTransactionException.class)
+	public ResponseEntity<Object> handleUnauthorizedTransactionException(UnauthorizedTransactionException ex) {
+	    return ResponseEntity
+	            .status(HttpStatus.FORBIDDEN)
+	            .body(Map.of(
+	                    "error", "FORBIDDEN",
+	                    "message", ex.getMessage(),
+	                    "details", "Transação não autorizada pelo serviço externo."
+	            ));
+	}   
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of(
-                        "error", "BAD_REQUEST",
-                        "message", ex.getMessage(),
-                        "details", "Uma requisição inválida foi enviada."
-                ));
-    }
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	public ResponseEntity<Object> handleMissingRequestParameter(MissingServletRequestParameterException ex) {
+	    return ResponseEntity
+	            .status(HttpStatus.BAD_REQUEST)
+	            .body(Map.of(
+	                    "error", "BAD_REQUEST",
+	                    "message", String.format("O parâmetro '%s' é obrigatório e não foi fornecido.", ex.getParameterName()),
+	                    "details", ex.getMessage()
+	            ));
+	}
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleGeneralException(Exception ex) {
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(Map.of(
-                        "error", "INTERNAL_SERVER_ERROR",
-                        "message", "Ocorreu um erro inesperado.",
-                        "details", ex.getMessage()
-                ));
-    }
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
+	    return ResponseEntity
+	            .status(HttpStatus.BAD_REQUEST)
+	            .body(Map.of(
+	                    "error", "BAD_REQUEST",
+	                    "message", ex.getMessage(),
+	                    "details", "Uma requisição inválida foi enviada."
+	            ));
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Object> handleGeneralException(Exception ex) {
+	    return ResponseEntity
+	            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+	            .body(Map.of(
+	                    "error", "INTERNAL_SERVER_ERROR",
+	                    "message", "Ocorreu um erro inesperado.",
+	                    "details", ex.getMessage()
+	            ));
+	}
 }
