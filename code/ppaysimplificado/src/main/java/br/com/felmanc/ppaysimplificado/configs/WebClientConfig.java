7b71965bedcfaca1f09f1fb7a4c62f94c935b860
felmanc <felmanc@gmail.com>
2025-03-23 11:29:36 -0300

    Ajustado WebClientConfig

--- code/ppaysimplificado/src/main/java/br/com/felmanc/ppaysimplificado/configs/WebClientConfig.java
+++ code/ppaysimplificado/src/main/java/br/com/felmanc/ppaysimplificado/configs/WebClientConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    WebClient webClient(WebClient.Builder builder) {
-        return builder.build();
+    WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 }
