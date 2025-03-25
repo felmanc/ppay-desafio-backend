86408118ff7f7d6a20e46b237ce851d887dd2ded
felmanc <felmanc@gmail.com>
2025-03-25 19:09:43 -0300

    Atualizado UserServiceTest

--- code/ppaysimplificado/src/test/java/br/com/felmanc/ppaysimplificado/units/services/UserServiceTest.java
+++ code/ppaysimplificado/src/test/java/br/com/felmanc/ppaysimplificado/units/services/UserServiceTest.java
@@ -17,12 +17,10 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.felmanc.ppaysimplificado.dtos.UserDTO;
