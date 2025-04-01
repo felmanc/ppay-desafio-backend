48f6a43aa332d9dcddc295fa5f3d38b1b5e4069b
felmanc <felmanc@gmail.com>
2025-04-01 13:01:33 -0300

    Atualizado UserTypeDeserializer

--- code/ppaysimplificado/src/main/java/br/com/felmanc/ppaysimplificado/converters/UserTypeDeserializer.java
+++ code/ppaysimplificado/src/main/java/br/com/felmanc/ppaysimplificado/converters/UserTypeDeserializer.java
@@ -7,10 +7,23 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
 import br.com.felmanc.ppaysimplificado.enums.UserType;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class UserTypeDeserializer extends JsonDeserializer<UserType> {
+
     @Override
     public UserType deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-        return UserType.fromString(p.getText());
+        String text = p.getText();
+        log.debug("[Deserialização] Texto obtido para deserialização: {}", text);
+
+        try {
+            UserType userType = UserType.fromString(text);
+            log.info("[Deserialização] UserType deserializado com sucesso: {}", userType);
+            return userType;
+        } catch (IllegalArgumentException e) {
+            log.error("[Erro] Falha na deserialização do UserType: {}", text, e);
+            throw e;
+        }
     }
 }
